aaaab7518452e752104e21f32dfda2d0f0b98449
cesiapbatista <cesiapbatista@192.168.68.53>
2024-11-21 16:58:42 -0400

    Se creo una clase fundamental que es el Historial Clinico
    Este permite ver la historia de un paciente en el sistema, dentro de la
    clínica.

---
+++ ClinicaProjectoFinal/src/models/HistoriaClinica.java
@@ -0,0 +1,34 @@
+package models;
+
+import java.util.ArrayList;
+
+public class HistoriaClinica {
+    private Paciente paciente;
+    private ArrayList<ConsultaMedica> consultas;
+
+    public HistoriaClinica(Paciente paciente) {
+        this.setPaciente(paciente);
+        this.consultas = new ArrayList<>();
+    }
+
+    public void agregarConsulta(ConsultaMedica consulta) {
+        consultas.add(consulta);
+    }
+
+    public String generarResumen() {
+        StringBuilder resumen = new StringBuilder();
+        resumen.append("Resumen de Historia Clínica:\n");
+        for (ConsultaMedica consulta : consultas) {
+            resumen.append(consulta.getFecha()).append(" - Diagnóstico: ").append(consulta.getDiagnostico()).append("\n");
+        }
+        return resumen.toString();
+    }
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+}
