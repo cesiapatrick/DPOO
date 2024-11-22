8047cf0950dfeab03e55db149e518a30b911a78f
cesiapbatista <cesiapbatista@192.168.68.53>
2024-11-22 15:28:56 -0400

    Controlador para las enfermedades
    Es una extension de la clase enfermedad donde se registra y se puede ver
    las enfermedades.

--- ClinicaProjectoFinal/src/controllers/EnfermedadController.java
+++ ClinicaProjectoFinal/src/controllers/EnfermedadController.java
@@ -1,5 +1,20 @@
 package controllers;
 
-public class EnfermedadController {
+import models.Enfermedad;
+import java.util.ArrayList;
 
+public class EnfermedadController {
+    private ArrayList<Enfermedad> enfermedades;
+
+    public EnfermedadController() {
+        this.enfermedades = new ArrayList<>();
+    }
+
+    public void registrarEnfermedad(Enfermedad enfermedad) {
+        enfermedades.add(enfermedad);
+    }
+
+    public ArrayList<Enfermedad> listarEnfermedades() {
+        return enfermedades;
+    }
 }
